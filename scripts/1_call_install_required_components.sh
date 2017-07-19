786e4c6ac82f6a021bcd5aff4620835576687ded
srinibadri <srini.badri01@gmail.com>
2017-07-19 16:44:36 -0700

    recursive copy and application stop

--- scripts/1_call_install_required_components.sh
+++ scripts/1_call_install_required_components.sh
@@ -1,4 +1,8 @@
 #!/bin/sh
 
+# Remove directories and files from previous installs
+sudo rm -rf ~/tmp
+sudo rm -f ~/*.log
+
 sudo ./install_required_components.sh 1>>/home/ubuntu/install_01.log 2>&1
 
--- scripts/2_applicationStop.sh
+++ scripts/2_applicationStop.sh
@@ -2,6 +2,4 @@
 
 exec 3>&1 1>>/home/ubuntu/install_02.log 2>&1
 
-# Remove directories and files from previous installs
-sudo rm -rf ~/tmp
-sudo rm -f ~/*.log
+# TBD - Stop running applications
