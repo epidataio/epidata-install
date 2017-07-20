4f01b418fb3ec0a20e466e27eaa6ba5e1f342821
srinibadri <srini.badri01@gmail.com>
2017-07-19 19:59:14 -0700

    cassandra replication

--- scripts/applicationStart_01.sh
+++ scripts/applicationStart_01.sh
@@ -5,24 +5,12 @@ echo "starting cassandra"
 cd /home/ubuntu/apache-cassandra-2.2.9
 screen -S cassandra -d -m  bash -c "bin/cassandra -f"
 
-/home/ubuntu/apache-cassandra-2.2.9/bin/cqlsh -u cassandra -p cassandra -e "ALTER KEYSPACE epidata_development WITH REPLICATION = { 'class' : 'SimpleStrategy', 'replication_factor' : 1 }"
-
-# Specify GitHub ID for default user
-export DEFAULT_USER_ID = ''
-
-# Add authorized users to Cassandra Users table
-echo "adding authorized users to database"
-echo `pwd`
-echo `which python`
-/home/ubuntu/apache-cassandra-2.2.9/bin/cqlsh -u cassandra -p cassandra -e "insert into epidata_development.users (id) values ($DEFAULT_USER_ID)"
-
 # Start Apache Spark
 echo "starting spark"
 export LOCAL_HOST_IP='127.0.0.1'
 export SPARK_HOME=/home/ubuntu/spark-2.1.0-bin-hadoop2.6
 SPARK_LOCAL_IP=$LOCAL_HOST_IP SPARK_MASTER_HOST=$LOCAL_HOST_IP SPARK_MASTER_WEBUI_PORT=18080 $SPARK_HOME/sbin/start-master.sh
 SPARK_LOCAL_IP=$LOCAL_HOST_IP  SPARK_WORKER_WEBUI_PORT=18081 $SPARK_HOME/sbin/start-slave.sh spark://$LOCAL_HOST_IP:7077 --host $LOCAL_HOST_IP
-
 
 # Start Apache Kafka
 cd /home/ubuntu/kafka_2.10-0.10.2.0
--- scripts/applicationStart_02.sh
+++ scripts/applicationStart_02.sh
@@ -1,5 +1,14 @@
 #!/bin/sh
 
+# Update Cassandra Replication Strategy
+/home/ubuntu/apache-cassandra-2.2.9/bin/cqlsh -u cassandra -p cassandra -e "ALTER KEYSPACE epidata_development WITH REPLICATION = { 'class' : 'SimpleStrategy', 'replication_factor' : 1 }"
+
+# Add authorized users to Cassandra Users table
+echo "adding authorized users to database"
+echo `pwd`
+echo `which python`
+/home/ubuntu/apache-cassandra-2.2.9/bin/cqlsh -u cassandra -p cassandra -e "insert into epidata_development.users (id) values ('DEFAULT_USER_ID')"
+
 # Launch Jupyter Notebook
 . /home/ubuntu/.profile
 cd /home/ubuntu/epidata/jupyter
